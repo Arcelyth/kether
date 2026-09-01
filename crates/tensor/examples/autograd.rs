add58663c01c586faf8f73479c0b1d1ba564b6dd
Arcelyth <arcelyth@tutamail.com>
2026-09-01 11:47:42 +0800

    update example use relu and update README

--- crates/tensor/examples/autograd.rs
+++ crates/tensor/examples/autograd.rs
@@ -3,12 +3,12 @@ use kether_tensor::{Tape, Tensor};
 fn main() {
     let mut tape = Tape::<f32>::new();
 
-    let x = Tensor::new(&[2.0, 3.0], [2], true, &mut tape);
+    let x = Tensor::new(&[-2.0, 3.0], [2], true, &mut tape);
     let y = Tensor::new(&[4.0, 5.0], [2], true, &mut tape);
-    let bias = Tensor::new(&[1.0, 1.0], [2], false, &mut tape);
+    let bias = Tensor::new(&[-1.0, 1.0], [2], false, &mut tape);
 
-    // z = x * y + bias
-    let z = x.mul(&y, &mut tape).add(&bias, &mut tape);
+    // z = ReLU(x * y + bias)
+    let z = x.mul(&y, &mut tape).add(&bias, &mut tape).relu(&mut tape);
     println!("z         = {:?}", z.data());
 
     z.backward(&mut tape);
